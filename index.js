b71b00834520b2ae587701b5761cb52d0cea456e
roshin <roshindavis15@gmail.com>
2025-01-02 04:35:09 +0530

    port issue

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
     dotenv.config();
     const app = express();
-    const port = process.env.PORT || 3000;
+    const port =  3000;
 
     app.use(cors({
         origin: ["https://pbs-dashboard.vercel.app", "*"],
