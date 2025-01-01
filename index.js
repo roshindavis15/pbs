56b7f4e8003c69bc10b813f38a8728e79f1f169b
roshin <roshindavis15@gmail.com>
2025-01-01 21:56:16 +0530

    cors setup enabled

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
     const app = express();
     const port = process.env.PORT || 3000;
 
-    app.use(cors())
+    app.use(cors({origin:["https://pbs-dashboard.vercel.app","*"]}))
     app.use(express.json());
     app.use('/admin', adminRouter);
 
