5a5181c771b1301211a1234290df351586afcb90
roshin <roshindavis15@gmail.com>
2025-01-02 22:50:45 +0530

    updated cors

--- index.js
+++ index.js
@@ -8,10 +8,11 @@
     const app = express();
     const port = process.env.PORT || 3000;
 
-    app.use(cors({
-        origin: process.env.FRONTEND_URL,
-        credentials:true
-      }));
+    // app.use(cors({
+    //     origin: process.env.FRONTEND_URL,
+    //     credentials:true
+    //   }));
+    app.use(cors());
     app.use(express.json());
     app.use('/admin', adminRouter);
 
